8fa25501d5d1bc238dde577d56a75d6eea4b2b32
Swachizen <swalehmohamed203@gmail.com>
2026-05-29 22:17:49 +0300

    Fix type

--- types/dashboard.ts
+++ types/dashboard.ts
@@ -1,5 +1,36 @@
 export type ViewState =
-  | "contacts"
+  | "dashboard"
   | "projects"
+  | "contacts"
   | "add-project"
   | "edit-project";
+
+export type Project = {
+  id: string;
+
+  title: string;
+
+  slug: string;
+
+  short_description: string;
+
+  problem_solved: string;
+
+  outcomes: string;
+
+  tech_stack: string[];
+
+  cover_image: string;
+
+  screenshots: string[];
+
+  live_demo: string;
+
+  github_link: string;
+
+  featured: boolean;
+
+  created_at: string;
+
+  updated_at: string;
+};
